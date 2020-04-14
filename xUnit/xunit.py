bb57b35dbca9920b020a550cb5305950f1b67d3a
ShotaKitazawa <skitazawa1121@gmail.com>
2020-04-15 00:51:04 +0900

    22章,23章

--- xUnit/xunit.py
+++ xUnit/xunit.py
@@ -5,19 +5,16 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
-    def run(self):
-        result = TestResult()
+    def run(self, result):
         result.testStarted()
         self.setUp()
-        method = getattr(self, self.name)
-        method()
-        # 以下だと意図しない例外を握りつぶしてしまうためNG
-        # try:
-        #    method()
-        # except Exception:
-        #    result.testFailed()
+        try:
+            method = getattr(self, self.name)
+            method()
+        except Exception as e:
+            print(e)
+            result.testFailed()
         self.tearDown()
-        return result
 
     def setUp(self):
         pass
@@ -29,17 +26,17 @@ class TestCase:
 class TestResult:
     def __init__(self):
         self.runCount = 0
-        self.failedCount = 0
+        self.errorCount = 0
 
     def testStarted(self):
         self.runCount += 1
 
     def testFailed(self):
-        self.failedCount += 1
+        self.errorCount += 1
 
     def summary(self):
-        return "{runCount} run, {failedCount} failed".format(
-            runCount=self.runCount, failedCount=self.failedCount
+        return "{runCount} run, {errorCount} failed".format(
+            runCount=self.runCount, errorCount=self.errorCount
         )
 
 
@@ -58,26 +55,59 @@ class WasRun(TestCase):
         raise Exception
 
 
+# TestSuite
+class TestSuite:
+    def __init__(self):
+        self.tests = []
+
+    def add(self, test):
+        self.tests.append(test)
+
+    def run(self, result):
+        for test in self.tests:
+            test.run(result)
+
+
 # TestCaseTest
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.result = TestResult()
+
     def testTemplateMethod(self):
         test = WasRun("testMethod")
-        test.run()
+        test.run(self.result)
         assert test.log == "setUp testMethod tearDown "
 
     def testResult(self):
         test = WasRun("testMethod")
-        result = test.run()
-        assert "1 run, 0 failed" == result.summary()
+        test.run(self.result)
+        assert "1 run, 0 failed" == self.result.summary()
 
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
-        result = test.run()
-        assert "1 run, 1 failed" == result.summary()
+        test.run(self.result)
+        assert "1 run, 1 failed" == self.result.summary()
+
+    def testFailedResultFormatting(self):
+        self.result.testStarted()
+        self.result.testFailed()
+        assert "1 run, 1 failed" == self.result.summary()
+
+    def testSuite(self):
+        suite = TestSuite()
+        suite.add(WasRun("testMethod"))
+        suite.add(WasRun("testBrokenMethod"))
+        suite.run(self.result)
+        assert "2 run, 1 failed" == self.result.summary()
 
 
 # Run Test
-TestCaseTest("testTemplateMethod").run()
-TestCaseTest("testResult").run()
-# raise より例外が発生するためpending
-# TestCaseTest("testFailedResult").run()
+suite = TestSuite()
+suite.add(TestCaseTest("testTemplateMethod"))
+suite.add(TestCaseTest("testResult"))
+suite.add(TestCaseTest("testFailedResult"))
+suite.add(TestCaseTest("testFailedResultFormatting"))
+suite.add(TestCaseTest("testSuite"))
+result = TestResult()
+suite.run(result)
+print(result.summary())
